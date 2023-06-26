5db8034ae906883e50aada44b0ca159bdbaafa3e
Renato <roliveira3891@gmail.com>
2023-06-26 16:38:49 -0300

    first commit

--- .history/src/app/layout_20230530230454.tsx
+++ .history/src/app/layout_20230530230454.tsx
@@ -2,11 +2,11 @@ import './globals.css'
 import { Metadata } from 'next'
 import ClientLayout from './client'
 import { APP_NAME } from '@/lib/consts'
-// import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import { Toaster } from 'react-hot-toast'
 import { FC, PropsWithChildren } from 'react'
 
-//const inter = Inter({ subsets: ['latin'], variable: '--font-inter', adjustFontFallback: false })
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter', adjustFontFallback: false })
 
 export const metadata = {
 	title: {
@@ -17,7 +17,7 @@ export const metadata = {
 
 const RootLayout: FC<PropsWithChildren<{}>> = ({ children }) => {
 	return (
-		<html lang="en">
+		<html lang="en" className={inter.variable}>
 			<body className="font-sans ">
 				<ClientLayout>{children}</ClientLayout>
 				<Toaster />
